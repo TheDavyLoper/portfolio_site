c9e435172b71fc02703a88df01ceb232378b9372
David Akpan <daviduwana@gmail.com>
2020-03-19 22:03:42 +0100

    add fontawesome close icon X for the mobile view

--- js/script.js
+++ js/script.js
@@ -4,8 +4,18 @@ const navUl = document.getElementById('nav-ul');
 
 const navLink = document.querySelectorAll('.nav__item');
 
+const close = document.getElementById('close-toggle')
+
 hamburger.addEventListener('click', ()=> {
   navUl.classList.toggle('show');
+  hamburger.style.display = "none";
+  close.style.display = "block"
+})
+
+close.addEventListener('click', ()=> {
+  navUl.classList.toggle('show');
+  close.style.display = "none";
+  hamburger.style.display = "block";
 })
 
 navLink.forEach(link => {
